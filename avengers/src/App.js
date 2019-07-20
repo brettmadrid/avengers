f865d442a8baa7feff9448e641691ee4e849de49
brettmadrid <brettmadrid9@gmail.com>
2019-07-20 02:09:55 -0700

    avengers data feed displayed in /avengers route

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Avengers from './components/Avengers';
+import avengers from './data';
 import './App.css';
 
 function App() {
+
+  const [ avengersData, setAvengersData ] = useState([])
+
+  useEffect(() => {
+    return () => {
+      setAvengersData(avengers)
+    };
+  }, [avengersData])
+
+  console.log('avengersData', avengersData)
+
   return (
     <div className="App">
-      
+      <Route exact path="/" component={Home} />
+      <Route path="/avengers" component={Avengers} />
     </div>
   );
 }
